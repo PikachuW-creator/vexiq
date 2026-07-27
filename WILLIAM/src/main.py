0e9161e550ba28512b7649fc7b5b6bd2d979d610
William <pikawilliam0304@gmail.com>
2026-07-27 15:36:34 +0900

    FEAT:VEX project init, linting connection

---
+++ WILLIAM/src/main.py
@@ -0,0 +1,17 @@
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+# 	Module:       main.py                                                      #
+# 	Author:       영재정보교육                                                       #
+# 	Created:      7/27/2026, 2:17:15 PM                                        #
+# 	Description:  IQ2 project                                                  #
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+
+# Library imports
+from vex import *
+
+# Brain should be defined by default
+brain=Brain()
+
+brain.screen.print("WILLIAM HELLO!")
+
