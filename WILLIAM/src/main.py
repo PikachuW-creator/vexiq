a0549f8fa4401120fddc30da76b5a30df12128ac
William <pikawilliam0304@gmail.com>
2026-08-03 14:34:19 +0900

    FEAT: Add Basebot drive control

--- WILLIAM/src/main.py
+++ WILLIAM/src/main.py
@@ -12,6 +12,18 @@ from vex import *
 
 # Brain should be defined by default
 brain=Brain()
+right_drive_motor = Motor(Ports.PORT6)
+left_drive_motor = Motor(Ports.PORT1)
 
-brain.screen.print("WILLIAM HELLO!")
+controller = Controller()
+#brain.screen.print("WILLIAM HELLO!")
 
+count = 0
+def main():
+    while True:
+        right_velocity = controller.axisD.position()
+        right_drive_motor.spin(REVERSE, right_velocity, PERCENT)
+        left_velocity = controller.axisA.position()
+        left_drive_motor.spin(FORWARD, left_velocity, PERCENT)
+
+main()
