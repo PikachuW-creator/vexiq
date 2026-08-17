20d3ab34acde0c69fbc6943972ac1b002218e9c6
William <pikawilliam0304@gmail.com>
2026-08-17 14:54:42 +0900

    #7 FEAT: add Joystick DEADBAND

--- WILLIAM/src/main.py
+++ WILLIAM/src/main.py
@@ -18,14 +18,20 @@ claw_motor = Motor(Ports.PORT4)
 
 controller = Controller()
 
+def apply_deadband(value):
+    if abs(value) < 15:
+        return 0 
+    else:
+        return value
+
 #brain.screen.print("WILLIAM HELLO!")
 
 
 def main():
     while True:
-        right_velocity = controller.axisD.position()
+        right_velocity = apply_deadband (controller.axisD.position())
         right_drive_motor.spin(REVERSE, right_velocity, PERCENT)
-        left_velocity = controller.axisA.position()
+        left_velocity = apply_deadband (controller.axisA.position())
         left_drive_motor.spin(FORWARD, left_velocity, PERCENT)
 
         right_button_pressed_up = controller.buttonRUp.pressing()
